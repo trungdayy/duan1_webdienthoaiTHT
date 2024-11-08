5f050b72fef58ce9b4c72dc7bf0ece6b4c8d9c91
VikeTus0201 <letuantu0123@gmail.com>
2024-11-08 10:09:05 +0700

    init project

--- commons/env.php
+++ commons/env.php
@@ -3,8 +3,8 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL'       , 'http://localhost/nhom8_DuAn1/nhom8/');
-define('BASE_URL_ADMIN' , 'http://localhost/nhom8_DuAn1/nhom8/admin/');
+define('BASE_URL'       , 'http://localhost/nhom8/THT/');
+define('BASE_URL_ADMIN' , 'http://localhost/nhom8/THT/admin/');
 
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
