e3e86657f8dcbb75c60cfb3fe205a7e40a31fdd9
hieuk5 <hieulvph51615@gmail.com>
2024-11-12 09:38:12 +0700

    add giao diên

--- admin/views/layout/sidebar.php
+++ admin/views/layout/sidebar.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="../../index3.html" class="brand-link">
-        <img src="./assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="./assets/dist/img/thtshop-removebg-preview.png" alt="Admin_THT" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Admin</span>
     </a>
 
